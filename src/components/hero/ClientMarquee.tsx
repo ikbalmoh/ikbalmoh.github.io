eb90ba5c31897436589f1109ad18711c4747baf5
Ikbal Moh <hikmat.iqbal@gmail.com>
2026-02-25 11:06:49 +0700

    feat: Make client logos clickable

--- src/components/hero/ClientMarquee.tsx
+++ src/components/hero/ClientMarquee.tsx
@@ -1,28 +1,35 @@
 import { useRef } from 'react'
 import styles from './styles.module.css'
 
-const clients: Array<string> = [
-  'tunezhub.svg',
-  'verity.svg',
-  'azyanna.webp',
-  'telkom.png',
-  'bmkg.png',
-  'pijar.png',
-  'dgti.png',
-  'ezy.png',
-  'selleri.png'
+interface Client {
+  name: string
+  url: string
+}
+
+const clients: Array<Client> = [
+  { name: 'tunezhub.svg', url: 'https://tunezhub.com' },
+  { name: 'verity.svg', url: 'https://www.verityhealth.io' },
+  { name: 'azyanna.webp', url: 'https://azyanna.com' },
+  { name: 'selleri.png', url: 'https://selleri.co.id' },
+  { name: 'dgti.png', url: 'https://dgti.co.id' },
+  { name: 'telkom.png', url: 'https://telkom.co.id' },
+  { name: 'pijar.png', url: 'https://pijarbelajar.id' },
+  { name: 'bmkg.png', url: 'https://bmkg.go.id' },
+  { name: 'ezy.png', url: 'https://id.linkedin.com/company/ezy-it-pte-ltd' },
 ]
 
 function Clients() {
   return (
     <div className={styles['marquee-track']}>
       {clients.map((client) => (
-        <img
-          key={client}
-          src={`/images/clients/${client}`}
-          alt="bmkg"
-          className="mx-3 h-10 w-auto object-contain"
-        />
+        <a href={client.url} target='_blank' className="mx-3 cursor-pointer">
+          <img
+            key={client.name}
+            src={`/images/clients/${client.name}`}
+            alt={client.name}
+            className="h-10 w-auto object-contain"
+          />
+        </a>
       ))}
     </div>
   )
@@ -34,12 +41,12 @@ const ClientMarquee = () => {
   return (
     <div
       ref={ref}
-      className="-z-10 mx-auto mt-5 flex w-full max-w-3xl flex-col items-center justify-center py-1 md:mt-10 md:flex-row md:items-center md:text-left"
+      className="relative mx-auto mt-5 flex w-full max-w-3xl flex-col items-center justify-center py-1 md:mt-10 md:flex-row md:items-center md:text-left"
     >
       <h2 className="mr-0 flex-1 whitespace-nowrap font-medium text-gray-700 md:mr-6 text-center md:text-left">
         Contributed for
       </h2>
-      <div className="relative mt-4 flex w-full overflow-hidden py-2 md:mt-0">
+      <div className={`relative mt-4 flex w-full overflow-hidden py-2 md:mt-0 ${styles['marquee-wrapper']}`}>
         <Clients />
         <Clients />
         <div className={styles['client-overlay']}></div>
